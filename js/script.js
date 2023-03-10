bce202e1ce9368f369c9ca76d8014b75c65f6a75
maggi-bratton <maggi.meins@gmail.com>
2023-03-10 10:51:26 -0700

    Changed clicked button styles

--- js/script.js
+++ js/script.js
@@ -4,6 +4,13 @@ var cat = document.querySelector(".cat");
 //console.log(cat);
 
 button.addEventListener("click", function () {
-//    console.log("It's working");
+if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add("disappear");
+} else {
     cat.classList.add("show");
+    button.innerText = "Shoo, cat!!";
+    button.classList.remove("disappear");
+}
 })
